b67834d6abd04c6b1d835b8d5332b0175a006e23
Sonya <sonya.karpovich@gmail.com>
2020-01-30 12:14:15 +0000

    Updates

---
+++ precache-manifest.73f7c1f89bdb7f677e986087d89bb7a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37150da104dde3d50e538ad3e1dfb673",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "456324bcbefc31f8a9ef",
+    "url": "/game-of-life/static/css/main.34212e49.chunk.css"
+  },
+  {
+    "revision": "0b10fd74f2865c12de5d",
+    "url": "/game-of-life/static/js/2.f77cb784.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/game-of-life/static/js/2.f77cb784.chunk.js.LICENSE"
+  },
+  {
+    "revision": "456324bcbefc31f8a9ef",
+    "url": "/game-of-life/static/js/main.7148563a.chunk.js"
+  },
+  {
+    "revision": "26936472eb00ba376096",
+    "url": "/game-of-life/static/js/runtime-main.35377514.js"
+  }
+]);
